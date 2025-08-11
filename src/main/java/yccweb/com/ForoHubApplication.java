54ca849690e07dad5f1df9d8224f82b8310dfeea
yober cieza <gctic.sgsass12@essalud.gob.pe>
2025-08-11 09:02:59 -0500

    inicializando el proyecto de foro hub

---
+++ src/main/java/yccweb/com/ForoHubApplication.java
@@ -0,0 +1,13 @@
+package yccweb.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForoHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yccweb/com/ForoHubApplicationTests.java
@@ -0,0 +1,13 @@
+package yccweb.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
